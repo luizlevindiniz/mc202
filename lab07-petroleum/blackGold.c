5afcb64c88ef92bb125774d7f78d601eb4093a6e
Luiz Diniz <luizd97@gmail.com>
2023-09-21 17:39:51 -0300

    lab08 added

--- lab07-petroleum/blackGold.c
+++ lab07-petroleum/blackGold.c
@@ -4,107 +4,40 @@
 int isUnique(int **matrix, int rows, int columns, int row, int column)
 {
     int isUnique = 0;
-    if (row == 0)
+    if (rows == 1 && columns == 1)
     {
-        if (column == 0)
+        isUnique = 1;
+    }
+    else if (rows == 1)
+    {
+        if (column == 0 && matrix[row][column + 1] == '*')
         {
-            if (matrix[row][column + 1] == '*' && matrix[row + 1][column] == '*' && matrix[row + 1][column + 1] == '*')
-            {
-                isUnique = 1;
-                return isUnique;
-            }
+            isUnique = 1;
         }
-
-        else if (column > 0 && column < columns - 1)
+        else if (column == columns - 1 && matrix[row][column - 1] == '*')
         {
-            if (matrix[row][column - 1] == '*' && matrix[row + 1][column - 1] == '*' &&
-                matrix[row + 1][column] == '*' && matrix[row][column + 1] == '*' &&
-                matrix[row + 1][column + 1] == '*')
-            {
-                isUnique = 1;
-                return isUnique;
-            }
+            isUnique = 1;
         }
-
-        else
+        else if (matrix[row][column + 1] == '*' && matrix[row][column - 1] == '*')
         {
-            if (matrix[row][column - 1] == '*' && matrix[row + 1][column - 1] == '*' &&
-                matrix[row + 1][column] == '*')
-            {
-                isUnique = 1;
-                return isUnique;
-            }
+            isUnique = 1;
         }
     }
-    else if (row == rows - 1)
+    else if (columns == 1)
     {
-        if (column == 0)
+        if (row == 0 && matrix[row + 1][column] == '*')
         {
-            if (matrix[row - 1][column] == '*' && matrix[row - 1][column + 1] == '*' && matrix[row][column + 1] == '*')
-            {
-                isUnique = 1;
-                return isUnique;
-            }
+            isUnique = 1;
         }
-
-        else if (column > 0 && column < columns - 1)
+        else if (row == rows - 1 && matrix[row + 1][column] == '*')
         {
-            if (matrix[row][column - 1] == '*' && matrix[row - 1][column - 1] == '*' &&
-                matrix[row - 1][column] == '*' && matrix[row - 1][column + 1] == '*' &&
-                matrix[row][column + 1] == '*')
-            {
-                isUnique = 1;
-                return isUnique;
-            }
+            isUnique = 1;
         }
-
-        else
+        else if (matrix[row + 1][column] == '*' && matrix[row - 1][column] == '*')
         {
-            if (matrix[row][column - 1] == '*' && matrix[row - 1][column - 1] == '*' &&
-                matrix[row - 1][column] == '*')
-            {
-                isUnique = 1;
-                return isUnique;
-            }
+            isUnique = 1;
         }
     }
-    else
-    {
-        if (column == 0)
-        {
-            if (matrix[row - 1][column] == '*' && matrix[row - 1][column + 1] == '*' &&
-                matrix[row][column + 1] == '*' && matrix[row + 1][column + 1] == '*' && matrix[row + 1][column] == '*')
-            {
-                isUnique = 1;
-                return isUnique;
-            }
-        }
-
-        else if (column > 0 && column < columns - 1)
-        {
-            if (matrix[row][column - 1] == '*' && matrix[row - 1][column - 1] == '*' &&
-                matrix[row - 1][column] == '*' && matrix[row - 1][column + 1] == '*' &&
-                matrix[row][column + 1] == '*' && matrix[row + 1][column + 1] == '*' &&
-                matrix[row + 1][column] == '*' && matrix[row + 1][column - 1] == '*')
-            {
-                isUnique = 1;
-                return isUnique;
-            }
-        }
-
-        else
-        {
-
-            if (matrix[row - 1][column] == '*' && matrix[row - 1][column - 1] == '*' &&
-                matrix[row][column - 1] == '*' && matrix[row + 1][column - 1] == '*' &&
-                matrix[row + 1][column] == '*')
-            {
-                isUnique = 1;
-                return isUnique;
-            }
-        }
-    }
-
     return isUnique;
 }
 
@@ -125,53 +58,63 @@ int main()
 {
     int oilReserves;
     int rows = 0, columns = 0;
-    scanf("%d %d", &rows, &columns);
+    int keepReading = 1;
 
-    oilReserves = 0;
-    int **oil = (int **)malloc(rows * sizeof(int *));
-    if (oil)
+    while (keepReading)
     {
-        for (int i = 0; i < rows; i++)
+        scanf("%d %d", &rows, &columns);
+        if (columns == 0)
         {
-            /* code */
-            oil[i] = (int *)malloc(columns * sizeof(int));
-
-            for (int j = 0; j < columns; j++)
-            {
-
-                scanf(" %c ", &(oil[i][j]));
-            }
+            keepReading = 0;
         }
-    }
-
-    printMatrix(oil, rows, columns);
-
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
+        else
         {
-            if (oil[i][j] == '@')
+
+            oilReserves = 0;
+            int **oil = (int **)malloc(rows * sizeof(int *));
+            if (oil)
             {
-                if (oilReserves == 0)
+                for (int i = 0; i < rows; i++)
                 {
-                    oilReserves++;
-                }
-                else
-                {
-                    // if (isUnique(oil, rows, columns, i, j))
-                    oilReserves++;
+                    /* code */
+                    oil[i] = (int *)malloc(columns * sizeof(int));
+
+                    for (int j = 0; j < columns; j++)
+                    {
+
+                        scanf(" %c", &(oil[i][j]));
+                    }
                 }
             }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (oil[i][j] == '@')
+                    {
+
+                        if (oilReserves == 0)
+                        {
+                            oilReserves++;
+                        }
+                        else if (isUnique(oil, rows, columns, i, j))
+                        {
+
+                            oilReserves++;
+                        }
+                    }
+                }
+            }
+            printf("%d\n", oilReserves);
+
+            for (int i = 0; i < rows; i++)
+            {
+                free(oil[i]);
+            }
+            free(oil);
         }
     }
-    printf("\n%d\n", oilReserves);
-
-    for (int i = 0; i < rows; i++)
-    {
-        free(oil[i]);
-    }
-    free(oil);
-    scanf("%d %d ", &rows, &columns);
 
     return 0;
 }
---
+++ lab08-matryoshka/matryoshka.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Stack
+{
+    /* data */
+    int top;
+    int maxSize;
+    int *stackArray;
+};
+
+struct Stack *createMatryoshka(int maxSize)
+{
+    struct Stack *matryoshka = malloc(sizeof(struct Stack));
+    matryoshka->top = -1;
+    matryoshka->maxSize = maxSize;
+    matryoshka->stackArray = malloc(sizeof(int) * maxSize);
+
+    return matryoshka;
+}
+
+int isFull(struct Stack *matryoshka)
+{
+    return matryoshka->maxSize == matryoshka->top + 1;
+}
+int isEmpty(struct Stack *matryoshka)
+{
+    return matryoshka->top == -1;
+}
+
+int topValue(struct Stack *matryoshka)
+{
+    if (!isEmpty(matryoshka))
+    {
+
+        return ((matryoshka->stackArray)[matryoshka->top]);
+    }
+
+    else
+    {
+        return -1;
+    }
+}
+
+void push(struct Stack *matryoshka, int doll)
+{
+    if (!isFull(matryoshka))
+    {
+        matryoshka->top = matryoshka->top + 1;
+        matryoshka->stackArray[matryoshka->top] = doll;
+    }
+    return;
+}
+
+int pop(struct Stack *matryoshka)
+{
+    if (!isEmpty(matryoshka))
+    {
+        int item = matryoshka->stackArray[matryoshka->top];
+        matryoshka->top = matryoshka->top - 1;
+        return item;
+    }
+    int empty = isEmpty(matryoshka);
+    return empty;
+}
+
+void printMatryoshka(struct Stack *matryoshka)
+{
+    for (int i = 0; i <= matryoshka->top; i++)
+    {
+        printf("%d ", matryoshka->stackArray[i]);
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+    int dollSize = 1;
+
+    while (dollSize > 0)
+    {
+
+        scanf("%d", &dollSize);
+
+        struct Stack *matryoshka = createMatryoshka(dollSize);
+        int isMatryoshka = 1;
+        int doll = 0;
+        int lastElement = 0;
+        int actualSize = 0;
+        for (int i = 0; i < dollSize; i++)
+        {
+            scanf("%d", &doll);
+
+            if (doll < 0 && i == 0)
+            {
+                actualSize++;
+                push(matryoshka, doll);
+            }
+            else if (doll < 0)
+            {
+
+                lastElement = topValue(matryoshka);
+                if (lastElement > doll)
+                {
+                    isMatryoshka = 0;
+                    break;
+                }
+                else
+                {
+                    actualSize++;
+                    push(matryoshka, doll);
+                }
+            }
+            else if (doll > 0 && i == 0)
+            {
+                isMatryoshka = 0;
+                break;
+            }
+            else
+            {
+                lastElement = topValue(matryoshka);
+                if (abs(lastElement) == doll)
+                {
+                    pop(matryoshka);
+                    actualSize--;
+                }
+                else
+                {
+                    isMatryoshka = 0;
+                    break;
+                }
+            }
+        }
+
+        if (isMatryoshka == 1)
+        {
+            printf(":-) Matriosca!\n");
+        }
+        else
+        {
+            printf(":-( Tente novamente.\n");
+        }
+        free(matryoshka->stackArray);
+        free(matryoshka);
+    }
+    return 0;
+}
